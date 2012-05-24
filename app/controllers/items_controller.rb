ba3757fe163e5351220d330061288594046cba5d
Roman Snitko <roman.snitko@gmail.com>
2012-05-24 04:41:13 +0400

    Edpisode 9: RESTful controllers, part 1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,38 @@
 class ItemsController < ApplicationController
 
+  Create
+  Read
+  Update
+  Destroy
+
   def index
     @items = Item.all
     render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
   end
 
+  # /items/1 GET
+  def show
+  end
+
+  # /items/new GET
+  def new
+  end
+
+  # /items/1/edit GET
+  def edit
+  end
+
+  # /items POST
   def create
     render text: "item created"
+  end
+
+  # /items/1 PUT
+  def update
+  end
+
+  # /items/1 DELETE
+  def destroy
   end
 
 end
