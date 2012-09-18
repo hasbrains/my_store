f8019c130468f6bd35fc9dd382479ad483458dcd
Roman Snitko <roman.snitko@gmail.com>
2012-09-18 07:30:49 +0700

    Episode 6: AR callbacks

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,13 @@ class Item < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
 
+  belongs_to :category
+
+  after_initialize { } # Item.new; Item.first
+  after_save       { } # Item.save; Item.create; item.update_attributes()
+  after_create     { category.inc(:items_count, 1) }
+  after_update     { }
+  after_destroy    { category.inc(:items_count, -1) } # item.destroy
+
+
 end
