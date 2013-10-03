ceda1bb50a6314757b31df081d78b0960ff185b5
Roman Snitko <roman.snitko@gmail.com>
2013-10-04 02:25:11 +0400

    Episode 44

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -17,5 +17,24 @@ describe ItemsController do
 
   end
 
+  describe "create action" do
+
+    it "redirects to image cropping page if validations pass" do
+      post :create, item: { name: 'Item 1', price: '10' }, admin: 1
+      response.should redirect_to(crop_image_item_path(assigns(:item)))
+    end
+
+    it "renders new page again if validations fail" do
+      post :create, item: { name: 'Item 1', price: 0 }, admin: 1
+      response.should render_template('new')
+    end
+
+    it "redners 403 page if user is not an admin" do
+      post :create, item: { name: 'Item 1', price: '10' }
+      response.status.should == 403
+    end
+
+  end
+
 
 end
