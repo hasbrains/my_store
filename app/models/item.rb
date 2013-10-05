dde731c71a9ec0baa53e0cc13dafcbe53788b7e8
Roman Snitko <roman.snitko@gmail.com>
2013-10-05 06:53:46 +0400

    Episode 45

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ActiveRecord::Base
   has_many :carts, through: :positions
 
   has_many :comments, as: :commentable
-  has_and_belongs_to_many :items
+  has_and_belongs_to_many :orders
 
   mount_uploader :image, ImageUploader
 
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,17 +2,30 @@ require 'spec_helper'
 
 describe ItemsController do
 
+  def self.it_renders_404_page_when_item_is_not_found(*actions)
+    actions.each do |a|
+      it "#{a} renders 404 page when item is not found" do
+        verb = if a == :update
+          "PATCH"
+        elsif a == :destroy
+          "DELETE"
+        else
+          "GET"
+        end
+        process a, verb, { id: 0 }
+        response.status.should == 404
+      end
+    end
+  end
+
+  it_renders_404_page_when_item_is_not_found :show, :edit, :update, :destroy
+
   describe "show action" do
 
     it "renders show template if an item is found" do
       item = create(:item)
       get :show, id: item.id
       response.should render_template('show')
-    end
-
-    it "renders 404 page if an item is not found" do
-      get :show, id: 0
-      response.status.should == 404
     end
 
   end
@@ -36,5 +49,15 @@ describe ItemsController do
 
   end
 
+
+  describe "destroy action" do
+
+    it "redirects to index action when an item is destroyed successfully" do
+      item = create(:item)
+      delete :destroy, id: item.id, admin: 1
+      response.should redirect_to(items_path)
+    end
+
+  end
 
 end
